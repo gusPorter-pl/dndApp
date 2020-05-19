72e1466074c6aad2e689507ee7ac4658e56dd8f7
Gus Porter <kiwiangus360@gmail.com>
2020-05-19 17:15:48 +1200

    Updated player type

--- src/services/types.ts
+++ src/services/types.ts
@@ -1,7 +1,7 @@
 export interface Player {
    key: number;
    name: string;
-   initiative: number;
+   initiative: number; // Make this optional?
    initiativeModifier?: number; // This will not be optional, need to update
    colour?: string; // Optional for initialisation of null, change to state.colours[0]?
 }
