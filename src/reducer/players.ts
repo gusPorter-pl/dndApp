af679af50dcc7a6342f8b0b14c0f2e19f2d2cdd1
Gus Porter <kiwiangus360@gmail.com>
2020-05-20 11:22:49 +1200

    Added reducer

---
+++ src/reducer/players.ts
@@ -0,0 +1,45 @@
+import {Player} from '../services/types';
+
+interface State {
+  players: Player[];
+  colours: string[];
+}
+
+const initialState: State = {
+  players: [],
+  colours: ['#00cf00', '#cf0000'],
+};
+
+type PlayerActionTypes =
+  | {type: 'addPlayer'; player: Player}
+  | {type: 'removePlayer'; player: Player}
+  | {type: 'editPlayer'; player: Player};
+
+export default function reducer(
+  state: State = initialState,
+  action: PlayerActionTypes,
+): State {
+  switch (action.type) {
+    case 'addPlayer':
+      return {
+        ...state,
+        players: [...state.players, action.player],
+      };
+    case 'removePlayer':
+      return {
+        ...state,
+        players: [...state.players.filter(player => player !== action.player)],
+      };
+    case 'editPlayer':
+      return {
+        ...state,
+        players: [
+          ...state.players.map(player =>
+            player !== action.player ? player : action.player,
+          ),
+        ],
+      };
+    default:
+      return state;
+  }
+}
