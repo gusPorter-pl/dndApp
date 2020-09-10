4431eab4db4c81b273a53e4f4d8bae5cd5205e5f
Gus Porter <apor417@aucklanduni.ac.nz>
2020-09-11 00:39:41 +1200

    Added home page

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,73 @@
 import React, {PureComponent} from 'react';
-import {StatusBar} from 'react-native';
+import {
+  StatusBar,
+  View,
+  TouchableOpacity,
+  Image,
+  Text,
+  StyleSheet
+} from 'react-native';
 import {Provider} from 'react-redux';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer, useNavigation} from '@react-navigation/native';
+
 import playerStore from './store/players';
 import Initiative from './views/initiative';
 import colours from './resources/colours';
+
+const emptyBox = require('./resources/assets/empty.jpg');
+
+const Stack = createStackNavigator();
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+  const pages = ['Players', 'Initiative'];
+  return (
+    <View style={{backgroundColor: '#dcdddf', flex: 1}}>
+      <View style={{marginTop: 20, alignItems: 'center'}}>
+        {pages.map((page) => {
+          return (
+            <View style={{marginTop: 5}} key={page}>
+              <TouchableOpacity
+                onPress={() => {
+                  navigation.navigate(page);
+                }}
+                activeOpacity={0.7}
+              >
+                <View style={styles.container}>
+                  <Image
+                    source={emptyBox}
+                    style={{width: 250, height: 60, resizeMode: 'stretch'}}
+                  />
+                  <View style={styles.overlapView}>
+                    <Text style={styles.overlapText}>{page}</Text>
+                  </View>
+                </View>
+              </TouchableOpacity>
+            </View>
+          );
+        })}
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    // flex: 1
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  overlapView: {
+    position: 'absolute',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  overlapText: {
+    color: 'black',
+    fontSize: 22
+  }
+});
 
 export default class App extends PureComponent {
   public constructor(props: {}) {
@@ -17,8 +81,39 @@ export default class App extends PureComponent {
           barStyle="light-content"
           backgroundColor={colours.backgroundColour}
         />
-        <Initiative />
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen
+              name="Home"
+              component={HomeScreen}
+              options={{
+                ...options,
+                headerTitleStyle: {
+                  ...options.headerTitleStyle,
+                  textAlign: 'center'
+                }
+              }}
+            />
+            <Stack.Screen
+              name="Initiative"
+              component={Initiative}
+              options={options}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
       </Provider>
     );
   }
 }
+
+const options = {
+  headerTintColor: '#fff',
+  headerStyle: {
+    backgroundColor: colours.backgroundColour
+  },
+  headerTitleStyle: {
+    fontSize: 25,
+    fontWeight: '100',
+    flex: 1
+  }
+};
