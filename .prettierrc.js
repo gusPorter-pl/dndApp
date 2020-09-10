4b1c7f39567ed9be61c4536747a84f9d1e7a7099
Gus Porter <apor417@aucklanduni.ac.nz>
2020-09-10 19:51:32 +1200

    Installed navigation dependencies

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,12 +1,12 @@
 module.exports = {
   bracketSpacing: false,
-	jsxBracketSameLine: false,
-	singleQuote: true,
-	semi: true,
-	useTabs: false,
-	tabWidth: 2,
-	arrowParens: 'always',
-	trailingComma: 'none'
+  jsxBracketSameLine: false,
+  singleQuote: true,
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  arrowParens: 'always',
+  trailingComma: 'none'
 };
 
 // npx prettier --config .prettierrc.js --write "src/**"
