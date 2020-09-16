76eb6b2f551a647a1ec291d399e4c07ac5cc4701
Gus Porter <apor417@aucklanduni.ac.nz>
2020-09-16 23:53:33 +1200

    added file requirements

--- src/common/spells.ts
+++ src/common/spells.ts
@@ -11,23 +11,28 @@ const spells = {
   },
   'chill-touch': {
     level: 0,
-    gif: true
+    gif: true,
+    image: require('../resources/spell-gifs/0/chill-touch.gif')
   },
   'dancing-lights': {
     level: 0,
-    gif: true
+    gif: true,
+    image: require('../resources/spell-gifs/0/dancing-lights.gif')
   },
   druidcraft: {
     level: 0,
-    gif: true
+    gif: true,
+    image: require('../resources/spell-gifs/0/druidcraft.gif')
   },
   'eldritch-blast': {
     level: 0,
-    gif: true
+    gif: true,
+    image: require('../resources/spell-gifs/0/eldritch-blast.gif')
   },
   'fire-bolt': {
     level: 0,
-    gif: true
+    gif: true,
+    image: require('../resources/spell-gifs/0/fire-bolt.gif')
   },
   friends: {
     level: 0,
@@ -36,67 +41,83 @@ const spells = {
   },
   guidance: {
     level: 0,
-    gif: true
+    gif: true,
+    image: require('../resources/spell-gifs/0/guidance.gif')
   },
   light: {
     level: 0,
-    gif: true
+    gif: true,
+    image: require('../resources/spell-gifs/0/light.gif')
   },
   'mage-hand': {
     level: 0,
-    gif: true
+    gif: true,
+    image: require('../resources/spell-gifs/0/mage-hand.gif')
   },
   mending: {
     level: 0,
-    gif: true
+    gif: true,
+    image: require('../resources/spell-gifs/0/mending.gif')
   },
   message: {
     level: 0,
-    gif: true
+    gif: true,
+    image: require('../resources/spell-gifs/0/message.gif')
   },
   'minor-illusion': {
     level: 0,
-    gif: true
+    gif: true,
+    image: require('../resources/spell-gifs/0/minor-illusion.gif')
   },
   'poison-spray': {
     level: 0,
-    gif: true
+    gif: true,
+    image: require('../resources/spell-gifs/0/poison-spray.gif')
   },
   prestidigitation: {
     level: 0,
-    gif: true
+    gif: true,
+    image: require('../resources/spell-gifs/0/prestidigitation.gif')
   },
   'produce-flame': {
     level: 0,
-    gif: true
+    gif: true,
+    image: require('../resources/spell-gifs/0/produce-flame.gif')
   },
   'ray-of-frost': {
     level: 0,
-    gif: true
+    gif: true,
+    image: require('../resources/spell-gifs/0/ray-of-frost.gif')
   },
   resistance: {
     level: 0,
-    gif: true
+    gif: true,
+    image: require('../resources/spell-gifs/0/resistance.gif')
   },
   'sacred-flame': {
     level: 0,
-    gif: true
+    gif: true,
+    image: require('../resources/spell-gifs/0/sacred-flame.gif')
   },
   shillelagh: {
     level: 0,
-    gif: true
+    gif: true,
+    image: require('../resources/spell-gifs/0/shillelagh.gif')
   },
   'shocking-grasp': {
     level: 0,
-    gif: true
+    gif: true,
+    image: require('../resources/spell-gifs/0/shocking-grasp.gif')
   },
   'spare-the-dying': {
     level: 0,
-    gif: true
+    gif: true,
+    image: require('../resources/spell-gifs/0/spare-the-dying.gif')
   },
   thaumaturgy: {
     level: 0,
-    gif: true
+    gif: true,
+    image: require('../resources/spell-gifs/0/thaumaturgy.gif')
   },
   'thorn-whip': {
     level: 0,
@@ -105,11 +126,13 @@ const spells = {
   },
   'true-strike': {
     level: 0,
-    gif: true
+    gif: true,
+    image: require('../resources/spell-gifs/0/true-strike.gif')
   },
   'vicious-mockery': {
     level: 0,
-    gif: true
+    gif: true,
+    image: require('../resources/spell-gifs/0/vicious-mockery.gif')
   }
 };
 
