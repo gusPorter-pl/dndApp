701b3808cae3be9c435ff546d4ffdba04c586fb6
Gus Porter <kiwiangus360@gmail.com>
2020-09-16 17:43:16 +1200

    Moved spells view

---
+++ src/views/spells/spells.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import {
+  ScrollView,
+  View,
+  Text,
+  Image,
+  Button,
+  TextInput,
+  TouchableOpacity
+} from 'react-native';
+import Header from '../../common/components/header';
+import Box from '../../common/components/box';
+
+import PageNavProps from '../navigation';
+import styles from '../../common/styles';
+import {kebabCaseConverter} from '../../common/functions';
+
+const spells = require('../../common/spells');
+const spellNames = Object.keys(spells);
+
+type Props = PageNavProps<'Spells'>;
+
+function Spells(props: Props) {
+  console.info(spellNames);
+  spellNames.forEach((spellName) => {
+    spells[spellName].displayName = kebabCaseConverter(spellName);
+  });
+  return (
+    <>
+      <Header title={props.route.name} />
+      <View style={styles.body}>
+        <TextInput style={styles.textInput} />
+        <ScrollView>
+          <View style={{flex: 1}}>
+            {spellNames.map((spell) => {
+              return (
+                <Box
+                  text={spells[spell].displayName}
+                  type={1}
+                  style={{paddingVertical: 3}}
+                  function={() => props.navigation.navigate('SpellDisplay')}
+                />
+              );
+            })}
+          </View>
+        </ScrollView>
+      </View>
+    </>
+  );
+}
+
+export default Spells;
