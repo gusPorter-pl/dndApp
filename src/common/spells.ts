8a82ae63b1655c2e4c358f05e7193e5214defb6b
Gus Porter <apor417@aucklanduni.ac.nz>
2020-09-17 21:16:54 +1200

    Added first level spells

--- src/common/spells.ts
+++ src/common/spells.ts
@@ -133,6 +133,316 @@ const spells = {
     level: 0,
     gif: true,
     image: require('../resources/spell-gifs/0/vicious-mockery.gif')
+  },
+  alarm: {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/alarm.gif')
+  },
+  'animal-friendship': {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/animal-friendship.gif')
+  },
+  'armour-of-agathys': {
+    level: 1,
+    gif: false,
+    'if-not-gif': {}
+  },
+  'arms-of-hadar': {
+    level: 1,
+    gif: false,
+    'if-not-gif': {}
+  },
+  bane: {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/bane.gif')
+  },
+  bless: {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/bless.gif')
+  },
+  'burning-hands': {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/burning-hands.gif')
+  },
+  'charm-person': {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/charm-person.gif')
+  },
+  'chromatic-orb': {
+    level: 1,
+    gif: false,
+    'if-not-gif': {}
+  },
+  'colour-spray': {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/colour-spray.gif')
+  },
+  command: {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/command.gif')
+  },
+  'compelled-duel': {
+    level: 1,
+    gif: false,
+    'if-not-gif': {}
+  },
+  'comprehend-languages': {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/comprehend-languages.gif')
+  },
+  'create-or-destroy-water': {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/create-or-destroy-water.gif')
+  },
+  'cure-wounds': {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/cure-wounds.gif')
+  },
+  'detect-evil-and-good': {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/detect-evil-and-good.gif')
+  },
+  'detect-magic': {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/detect-magic.gif')
+  },
+  'detect-poison-and-disease': {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/detect-poison-and-disease.gif')
+  },
+  'disguise-self': {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/disguise-self.gif')
+  },
+  'dissonant-whispers': {
+    level: 1,
+    gif: false,
+    'if-not-gif': {}
+  },
+  'divine-favour': {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/divine-favour.gif')
+  },
+  'ensnaring-strike': {
+    level: 1,
+    gif: false,
+    'if-not-gif': {}
+  },
+  entangle: {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/entangle.gif')
+  },
+  'expeditious-retreat': {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/expeditious-retreat.gif')
+  },
+  'faerie-fire': {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/faerie-fire.gif')
+  },
+  'false-life': {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/false-life.gif')
+  },
+  'feather-fall': {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/feather-fall.gif')
+  },
+  'find-familiar': {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/find-familiar.gif')
+  },
+  'fog-cloud': {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/fog-cloud.gif')
+  },
+  goodberry: {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/goodberry.gif')
+  },
+  grease: {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/grease.gif')
+  },
+  'guiding-bolt': {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/guiding-bolt.gif')
+  },
+  'hail-of-thorns': {
+    level: 1,
+    gif: false,
+    'if-not-gif': {}
+  },
+  'healing-word': {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/healing-word.gif')
+  },
+  'hellish-rebuke': {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/hellish-rebuke.gif')
+  },
+  heroism: {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/heroism.gif')
+  },
+  hex: {
+    level: 1,
+    gif: false,
+    'if-not-gif': {}
+  },
+  "hunter's-mark": {
+    level: 1,
+    gif: true,
+    image: require("../resources/spell-gifs/1/hunter's-mark.gif")
+  },
+  identify: {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/identify.gif')
+  },
+  'illusory-script': {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/illusory-script.gif')
+  },
+  'inflict-wounds': {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/inflict-wounds.gif')
+  },
+  jump: {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/jump.gif')
+  },
+  longstrider: {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/longstrider.gif')
+  },
+  'mage-armour': {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/mage-armour.gif')
+  },
+  'magic-missile': {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/magic-missile.gif')
+  },
+  'protection-from-evil-and-good': {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/protection-from-evil-and-good.gif')
+  },
+  'purify-food-and-drink': {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/purify-food-and-drink.gif')
+  },
+  'ray-of-sickness': {
+    level: 1,
+    gif: false,
+    'if-not-gif': {}
+  },
+  sanctuary: {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/sanctuary.gif')
+  },
+  'searing-smite': {
+    level: 1,
+    gif: false,
+    'if-not-gif': {}
+  },
+  shield: {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/shield.gif')
+  },
+  'shield-of-faith': {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/shield-of-faith.gif')
+  },
+  'silent-image': {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/silent-image.gif')
+  },
+  sleep: {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/sleep.gif')
+  },
+  'speak-with-animals': {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/speak-with-animals.gif')
+  },
+  "tasha's-hideous-laughter": {
+    level: 1,
+    gif: true,
+    image: require("../resources/spell-gifs/1/tasha's-hideous-laughter.gif")
+  },
+  "tenser's-floating-disk": {
+    level: 1,
+    gif: true,
+    image: require("../resources/spell-gifs/1/tenser's-floating-disk.gif")
+  },
+  'thunderous-smite': {
+    level: 1,
+    gif: false,
+    'if-not-gif': {}
+  },
+  thunderwave: {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/thunderwave.gif')
+  },
+  'unseen-servant': {
+    level: 1,
+    gif: true,
+    image: require('../resources/spell-gifs/1/unseen-servant.gif')
+  },
+  'witch-bolt': {
+    level: 1,
+    gif: false,
+    'if-not-gif': {}
+  },
+  'wrathful-smite': {
+    level: 1,
+    gif: false,
+    'if-not-gif': {}
   }
 };
 
