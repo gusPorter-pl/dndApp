dad10893593f55f34a854d39d093ba92f6ad5da4
Gus Porter <apor417@aucklanduni.ac.nz>
2020-09-17 21:39:59 +1200

    Added SafeAreaView

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import {StatusBar} from 'react-native';
 import {Provider} from 'react-redux';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 import playerStore from './store/players';
 import TabScreen from './views';
@@ -20,13 +21,18 @@ export default class App extends PureComponent {
   public render() {
     return (
       <Provider store={playerStore}>
-        <StatusBar barStyle="light-content" backgroundColor={colours.header} />
-        <NavigationContainer>
-          <Stack.Navigator headerMode="none">
-            <Stack.Screen name="Tabs" component={TabScreen} />
-            <Stack.Screen name="SpellDisplay" component={SpellDisplay} />
-          </Stack.Navigator>
-        </NavigationContainer>
+        <SafeAreaProvider>
+          <StatusBar
+            barStyle="light-content"
+            backgroundColor={colours.header}
+          />
+          <NavigationContainer>
+            <Stack.Navigator headerMode="none">
+              <Stack.Screen name="Tabs" component={TabScreen} />
+              <Stack.Screen name="SpellDisplay" component={SpellDisplay} />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </SafeAreaProvider>
       </Provider>
     );
   }
