0dded4aa2c4239f7a09d30063415beb19479b19d
Gus Porter <apor417@aucklanduni.ac.nz>
2020-09-21 14:55:14 +1200

    Moved stack screens out of index

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,11 @@ import React, {PureComponent} from 'react';
 import {StatusBar} from 'react-native';
 import {Provider} from 'react-redux';
 import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
+import StackScreen from './views';
 import store from './redux/store';
-import TabScreen from './views';
 import colours from './common/colours';
-import {MainParamList} from './navigation';
-import SpellDisplay from './spellDisplay';
-
-const Stack = createStackNavigator<MainParamList>();
 
 export default class App extends PureComponent {
   public constructor(props: {}) {
@@ -27,10 +22,7 @@ export default class App extends PureComponent {
             backgroundColor={colours.header}
           />
           <NavigationContainer>
-            <Stack.Navigator headerMode="none">
-              <Stack.Screen name="Tabs" component={TabScreen} />
-              <Stack.Screen name="SpellDisplay" component={SpellDisplay} />
-            </Stack.Navigator>
+            <StackScreen />
           </NavigationContainer>
         </SafeAreaProvider>
       </Provider>
