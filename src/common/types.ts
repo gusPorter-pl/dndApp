d8801e8ea998468f2ea2c84fc2692812df453ce4
Gus Porter <kiwiangus360@gmail.com>
2020-09-25 12:11:36 +1200

    Updated Player type

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,9 +1,8 @@
 export interface Player {
   name: string;
-  initiative?: number; // Make this optional?
-  initiativeModifier?: number; // This will not be optional, need to update
-  reaction: boolean; // Replaced colour
-  // colour: number; // This references the index in state.colours array
+  initiative?: number;
+  initiativeModifier: number;
+  reaction: boolean;
   characterType: 'PC' | 'NPC';
   // legendaryActions?: number;
 }
