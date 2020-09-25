a0e6216c27c88677639aed5e1c0d63a38d78e0b8
Gus Porter <kiwiangus360@gmail.com>
2020-09-25 14:57:52 +1200

    Added functionality to initiative order

--- src/views/initiativeOrder.tsx
+++ src/views/initiativeOrder.tsx
@@ -1,5 +1,5 @@
 import React, {PureComponent} from 'react';
-import {View, Text, Image} from 'react-native';
+import {View, Text, Image, ScrollView, TextInput} from 'react-native';
 import {connect} from 'react-redux';
 
 import MainNavProps from './navigation';
@@ -12,9 +12,11 @@ import styles from '../common/styles';
 import {Player} from '../common/types';
 import Box from '../common/components/box';
 import Header from '../common/components/header';
+import players from './tabs/players';
 
 interface LocalState {
   players: Player[];
+  allFilled: boolean;
 }
 
 interface StateProps {}
@@ -28,21 +30,74 @@ class InitiativeOrder extends PureComponent<Props, LocalState> {
     super(props);
     const players = this.props.route.params.players;
     this.state = {
-      players
+      players,
+      allFilled: true
     };
   }
-
-  // public componentDidMount() {
-  //   this.props.navigation.addListener('blur', () => {
-  //   });
-  // }
 
   public render() {
     return (
       <>
         <Header title="Initiative Order" />
         <View style={styles.body}>
-          <Text>{JSON.stringify(this.state.players)}</Text>
+          <ScrollView>
+            {this.state.players.map((player) => {
+              return (
+                <View>
+                  <Box text={player.name} type={0} />
+                  <View style={{alignItems: 'center'}}>
+                    <TextInput
+                      style={[styles.textInput, {width: '90%'}]}
+                      placeholder="Initiative"
+                      defaultValue=""
+                      keyboardType="numeric"
+                      onChangeText={(value) => {
+                        this.setState({
+                          players: this.state.players.map((otherPlayer) =>
+                            otherPlayer.name !== player.name
+                              ? otherPlayer
+                              : {...player, initiative: parseInt(value)}
+                          )
+                        });
+                      }}
+                    />
+                  </View>
+                </View>
+              );
+            })}
+            <View style={{paddingTop: 10, paddingHorizontal: 10}}>
+              <Box
+                text="Start Initiative"
+                type={0}
+                function={async () => {
+                  // this.props.navigation.goBack();
+                  for (const player of this.state.players) {
+                    if (!player.initiative) {
+                      await this.setState({allFilled: false});
+                      break;
+                    }
+                  }
+                  if (this.state.allFilled) {
+                    console.log('To initiative!');
+                  }
+                }}
+              />
+              {!this.state.allFilled && (
+                <View style={{paddingVertical: 3}}>
+                  <Text style={{textAlign: 'center', fontSize: 16}}>
+                    Fill in all of the boxes!
+                  </Text>
+                </View>
+              )}
+              <Box
+                text="Cancel and Go Back"
+                type={0}
+                function={() => {
+                  this.props.navigation.goBack();
+                }}
+              />
+            </View>
+          </ScrollView>
         </View>
       </>
     );
