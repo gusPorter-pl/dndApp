0852fa54c851eb53c3d468be477dfaae64daed13
Gus Porter <kiwiangus360@gmail.com>
2020-09-25 14:48:34 +1200

    Removed unnecessary import

--- src/common/components/box.tsx
+++ src/common/components/box.tsx
@@ -1,12 +1,5 @@
 import React from 'react';
-import {
-  View,
-  Text,
-  TouchableOpacity,
-  Image,
-  ViewStyle,
-  StyleSheet
-} from 'react-native';
+import {View, Text, TouchableOpacity, Image, StyleSheet} from 'react-native';
 
 import styles from '../styles';
 
